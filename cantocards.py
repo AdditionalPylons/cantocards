ad093b8e0c19119c389ead36914f7553eed9d071
Bill <bill@popstack.io>
2022-06-09 19:43:41 -0500

    Major refactor, async update

--- cantocards.py
+++ cantocards.py
@@ -1,11 +1,141 @@
+import aiohttp
+import asyncio
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from pathlib import Path
 from urllib.request import urlretrieve
 from typing import List
+from bs4 import BeautifulSoup
 
 import csv
+import re
+
+def parse_search_results(search_results):
+    complete = []
+    incomplete = []
+    missing = []
+    for term, raw_html in search_results:
+        soup = BeautifulSoup(raw_html, "html.parser")
+
+        no_matches = re.compile(r"Number of matches: 0")
+        eng_def_regex = re.compile(r">english:</span>\xa0(<em>)?(?P<english>[A-Za-z\d\s,'-=/()&;\[\]]+)</em>?[^a-zA-Z\d\s:]")
+        if no_matches.search(str(soup.body)):
+            missing.append(term)
+            continue
+
+        parsed = {
+            "mandarin": term,
+            "character": soup.body.b.string if soup.body.b else "MISSING_DATA",
+            "english": eng_def_regex.search(str(soup.body)).group("english").strip() if eng_def_regex.search(str(soup.body)) else "MISSING_DATA",
+            "audio": soup.body.a["href"] if soup.body.a else "MISSING_DATA",
+        }
+        if "MISSING_DATA" in parsed.values():
+            incomplete.append(parsed)
+        else:
+            complete.append(parsed)
+
+    final = {
+        "complete": complete,
+        "incomplete": incomplete,
+        "missing": missing,
+    }
+    len_final = len(final["complete"]) + len(final["incomplete"]) + len(final["missing"])
+    print(f'COMPLETE: {final["complete"]}, {(len(final["complete"])/len_final)*100}%\n')
+    print(f'INCOMPLETE: {final["incomplete"]}, {(len(final["incomplete"])/len_final)*100}%\n')
+    print(f'MISSING: {final["missing"]}, {(len(final["missing"])/len_final)*100}%\n')
+    return final
+
+async def search(terms):
+
+    async with aiohttp.ClientSession() as session:
+        results = []
+        no_eng = 0
+        for term in terms:
+            url = "https://www.stephen-li.com/TaishaneseVocabulary/MySQLSearch.php"
+            payload = {
+                "data": term,
+                "Select1": "Mandarin",
+                "submit": "Search",
+                "Select2": "Full",
+            }
+            form_data = aiohttp.FormData()
+            for k, v in payload.items():
+                form_data.add_field(k, v)
+            async with session.post(url, data=form_data) as response:
+                print(f"Searching for {term}...")
+                html = await response.text()
+                results.append((term, html))
+        parsed = parse_search_results(results)
+    return parsed
+
+def import_vocab_list(file):
+
+    with open(Path.cwd().joinpath(file), "r") as source:
+        results = [line.strip() for line in source]
+        return results
+
+def scrape_hsk(hsk_level):
+
+    levels = {
+        1: "HSK1.txt",
+        2: "HSK2.txt",
+        3: "HSK3.txt",
+        4: "HSK4.txt",
+        5: "HSK5.txt",
+        6: "HSK6.txt",
+        }
+
+    if hsk_level == 0 or hsk_level == "all":
+        full_hsk_list = []
+        for level in levels.values():
+            full_hsk_list += [*import_vocab_list(level)]
+        print(full_hsk_list)
+        asyncio.run(search(full_hsk_list))
+    else:
+        asyncio.run(search(import_vocab_list(levels[hsk_level])))
+
+"""
+OLD CODE BELOW
+# TODO: REFACTOR THIS FOR THE NEW FLOW
+
+def export_entries(self, filename="scraped_results", extension="txt"):
+
+    with open(Path.cwd().joinpath(f"{filename}.{extension}"),
+            "a+") as file:
+
+        def write_entry(word, definition):
+            if extension == "txt":
+                file.write(f"{word} : {definition}\n")
+            elif extension == "csv":
+                writer = csv.writer(file, delimiter=',', quotechar='"',
+                                    quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([f"{word}", f"{definition}"])
+
+        for key, value in self.scraped_results.items():
+            write_entry(key, value[0])
+
+def export_audio(self, folder_name="audio"):
+    audio_dir = Path.cwd().joinpath(folder_name)
+    if not audio_dir.exists():
+        audio_dir.mkdir()
+
+    for word, results in self.scraped_results.items():
+            url = results[1]
+            filepath = audio_dir.joinpath(f"{word}.mp3")
+            if not filepath.exists():
+                urlretrieve(url, filepath)
+
+def print_results(self):
+    for key, value in self.scraped_results.items():
+        print(f"TERM: {key} DEF: {value[0]} CLIP: {value[1]}")
+
+"""
+
+asyncio.run(search(["主义"]))
+#scrape_hsk(0)
+
 
 
 class TaishaneseScraper():
@@ -142,4 +272,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    ...
+    # main()
